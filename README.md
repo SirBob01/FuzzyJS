e4f7c4b07e3a3f612e3ccb680893b089016116e7
Keith Leonardo <kleonardo52@gmail.com>
2019-10-18 19:27:56 +1100

    Update README.md
    Add documentation for using fuzzy library. Change
    tabs to spaces.

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
 # FuzzyJS
 
-A lightweight fuzzy search algorithm.
+A lightweight fuzzy search algorithm that calculates a weighted similarity score between two strings.
 
+# Dependencies
+
+None. Pure JavaScript.
+
+# Basic Usage
+
+1. Create a new `Fuzzy()` object.
+```js
+var fuzzy = new Fuzzy();
+```
+
+2. Set the parameters for the search.
+```js
+fuzzy.options = {
+    sort : true,            // Sort result by score
+    n_size : 3,             // N-gram size
+    min_query : 2,          // Minimum query length
+    min_similarity : 0.6,   // Threshold similarity score
+    all_matches : true      // Return all potential matches?
+};
+```
+
+3. Index the search space into the fuzzy engine.
+```js
+var keys = ["Hello", "Helper", "world", "bob"];
+fuzzy.index(keys);
+```
+
+4. Search for a query and get an array of matches.
+```js
+var results = fuzzy.search(keys, "Hel"); // ["Hello", "Helper"]
+```
--- src/fuzzy.js
+++ src/fuzzy.js
@@ -1,200 +1,200 @@
 function Fuzzy() {
-	var self = this; // Keep context of parent prototype
-	self.gram_db = {};
-	self.options = {
-		sort : true,
-		n_size : 3,
-		min_query : 2,
-		min_similarity : 0.6,
-		all_matches : false
-	};
+    var self = this; // Keep context of parent prototype
+    self.gram_db = {};
+    self.options = {
+        sort : true,
+        n_size : 3,
+        min_query : 2,
+        min_similarity : 0.6,
+        all_matches : false
+    };
 
-	self.normalize = function(string) {
-		// Removes all non-alphanumeric characters
-		return string.toLowerCase().replace(/(\W)+/g,'');
-	}
+    self.normalize = function(string) {
+        // Removes all non-alphanumeric characters
+        return string.toLowerCase().replace(/(\W)+/g,'');
+    }
 
-	self.index = function(dict) {
-		// Pre-process dictionary for fast n-gram search
-		self.gram_db = {};
-		var n = self.options.n_size;
+    self.index = function(dict) {
+        // Pre-process dictionary for fast n-gram search
+        self.gram_db = {};
+        var n = self.options.n_size;
 
-		for(var i = 0; i < dict.length; i++) {	
-			var phrase = self.normalize(dict[i]);
-			for(var j = 0; j < phrase.length-n+1; j++) {
-				var gram = phrase.slice(j, j+n);
-				if(gram in self.gram_db && phrase in self.gram_db[gram]) {
-					self.gram_db[gram][phrase]++;
-				}
-				else {
-					self.gram_db[gram] = {};
-					self.gram_db[gram][phrase] = 1;
-				}
-			}
-		}
-	}
+        for(var i = 0; i < dict.length; i++) {    
+            var phrase = self.normalize(dict[i]);
+            for(var j = 0; j < phrase.length-n+1; j++) {
+                var gram = phrase.slice(j, j+n);
+                if(gram in self.gram_db && phrase in self.gram_db[gram]) {
+                    self.gram_db[gram][phrase]++;
+                }
+                else {
+                    self.gram_db[gram] = {};
+                    self.gram_db[gram][phrase] = 1;
+                }
+            }
+        }
+    }
 
-	self.gram_counter = function(phrase) {
-		// Calculates local n-grams of query phrase
-		var phrase = self.normalize(phrase);
-		var grams = {};
-		var n = self.options.n_size;
+    self.gram_counter = function(phrase) {
+        // Calculates local n-grams of query phrase
+        var phrase = self.normalize(phrase);
+        var grams = {};
+        var n = self.options.n_size;
 
-		for(var i = 0; i < phrase.length-n+1; i++) {
-			g = phrase.slice(i, i+n);
-			if(g in grams) {
-				grams[g]++;
-			}
-			else {
-				grams[g] = 1;
-			}
-		}
-		return grams;
-	}
+        for(var i = 0; i < phrase.length-n+1; i++) {
+            g = phrase.slice(i, i+n);
+            if(g in grams) {
+                grams[g]++;
+            }
+            else {
+                grams[g] = 1;
+            }
+        }
+        return grams;
+    }
 
-	self.damlev_distance = function(a, b) {
-		// Damerau-Levenshtein distance
-		var a = a.toLowerCase();
-		var b = b.toLowerCase();
+    self.damlev_distance = function(a, b) {
+        // Damerau-Levenshtein distance
+        var a = a.toLowerCase();
+        var b = b.toLowerCase();
 
-		var m = a.length;
-		var n = b.length;
-		
-		if(m == 0) return n;
-		if(n == 0) return m;
+        var m = a.length;
+        var n = b.length;
+        
+        if(m == 0) return n;
+        if(n == 0) return m;
 
-		var matrix = new Array(m+1)
-		for(var i = 0; i <= m; i++) {
-			matrix[i] = new Array(n+1).fill(0);
-		}
-		
-		for(var i = 1; i <= m; i++) {
-			matrix[i][0] = i;
-		}
-		for(var j = 1; j <= n; j++) {
-			matrix[0][j] = j;
-		}
-		
-		for(var j = 1; j <= n; j++) {
-			for(var i = 1; i <= m; i++) {
-				if(a[i-1] == b[j-1]) {
-					cost = 0;
-				}
-				else {
-					cost = 1;
-				}
-				matrix[i][j] = Math.min(matrix[i-1][j]+1,
-										matrix[i][j-1]+1,
-										matrix[i-1][j-1]+cost
-				);
-				if(i > 1 && j > 1 && a[i-1] == b[j-2] && a[i-2] == b[j-1]) {
-					matrix[i][j] = Math.min(matrix[i][j],
-									   		matrix[i-2][j-2]+cost
-					);
-				}
-			}
-		}
+        var matrix = new Array(m+1)
+        for(var i = 0; i <= m; i++) {
+            matrix[i] = new Array(n+1).fill(0);
+        }
+        
+        for(var i = 1; i <= m; i++) {
+            matrix[i][0] = i;
+        }
+        for(var j = 1; j <= n; j++) {
+            matrix[0][j] = j;
+        }
+        
+        for(var j = 1; j <= n; j++) {
+            for(var i = 1; i <= m; i++) {
+                if(a[i-1] == b[j-1]) {
+                    cost = 0;
+                }
+                else {
+                    cost = 1;
+                }
+                matrix[i][j] = Math.min(matrix[i-1][j]+1,
+                                        matrix[i][j-1]+1,
+                                        matrix[i-1][j-1]+cost
+                );
+                if(i > 1 && j > 1 && a[i-1] == b[j-2] && a[i-2] == b[j-1]) {
+                    matrix[i][j] = Math.min(matrix[i][j],
+                                               matrix[i-2][j-2]+cost
+                    );
+                }
+            }
+        }
 
-		return matrix[m-1][n-1];
-	}
+        return matrix[m-1][n-1];
+    }
 
-	self.n_gram = function(key, query) {
-		// Compares word structure of two sentences
-		// Vectorizes strings and calculates cosine similarity
-		// Returns (a.b)/(|a||b|)
-		var q_grams = self.gram_counter(query);
-		var k_phrase = self.normalize(key);
-		
-		var dot = 0;
-		var a_mag = 0;
-		var b_mag = 0;
+    self.n_gram = function(key, query) {
+        // Compares word structure of two sentences
+        // Vectorizes strings and calculates cosine similarity
+        // Returns (a.b)/(|a||b|)
+        var q_grams = self.gram_counter(query);
+        var k_phrase = self.normalize(key);
+        
+        var dot = 0;
+        var a_mag = 0;
+        var b_mag = 0;
 
-		for(var g in q_grams) {
-			if(g in self.gram_db && k_phrase in self.gram_db[g]) {
-				dot += q_grams[g]*self.gram_db[g][k_phrase];
+        for(var g in q_grams) {
+            if(g in self.gram_db && k_phrase in self.gram_db[g]) {
+                dot += q_grams[g]*self.gram_db[g][k_phrase];
 
-				a_mag += Math.pow(q_grams[g], 2);
-				b_mag += Math.pow(self.gram_db[g][k_phrase], 2);
-			}
-		}
-		if(a_mag*b_mag == 0) return 0; // Completely different strings
-		else return dot/Math.sqrt(a_mag*b_mag);
-	}
+                a_mag += Math.pow(q_grams[g], 2);
+                b_mag += Math.pow(self.gram_db[g][k_phrase], 2);
+            }
+        }
+        if(a_mag*b_mag == 0) return 0; // Completely different strings
+        else return dot/Math.sqrt(a_mag*b_mag);
+    }
 
-	self.edit_similarity = function(key, query) {
-		// Normalize
-		var k_t = key.split(" ");
-		var q_t = query.split(" ");
+    self.edit_similarity = function(key, query) {
+        // Normalize
+        var k_t = key.split(" ");
+        var q_t = query.split(" ");
 
-		// b_t needs to be the shorter sequence
-		if(q_t.length > k_t.length) {
-			var temp = q_t;
-			q_t = k_t;
-			k_t = temp;
-		}
+        // b_t needs to be the shorter sequence
+        if(q_t.length > k_t.length) {
+            var temp = q_t;
+            q_t = k_t;
+            k_t = temp;
+        }
 
-		var long = k_t.length;
-		var short = q_t.length;
+        var long = k_t.length;
+        var short = q_t.length;
 
-		var similarity = 0; // Assume strings are completely different
-		// Not sure how to optimize, worst case is O(long*short)
-		// Negligible for short queries, I guess?
-		for(var i = 0; i < long-short+1; i++) {
-			var substring = k_t.slice(i, i+short);
-			var local_distance = 0;
-			for(var j = 0; j < short; j++) {
-				var dist = self.damlev_distance(substring[j], q_t[j]);
-				var norm = Math.max(substring[j].length, q_t[j].length);
-				local_distance += dist/norm;
-			}
-			if(local_distance/short < 1-self.options.min_similarity) {
-				similarity = 1-(local_distance/short);
-				break;
-			}
-		}
-		return similarity;
-	}
+        var similarity = 0; // Assume strings are completely different
+        // Not sure how to optimize, worst case is O(long*short)
+        // Negligible for short queries, I guess?
+        for(var i = 0; i < long-short+1; i++) {
+            var substring = k_t.slice(i, i+short);
+            var local_distance = 0;
+            for(var j = 0; j < short; j++) {
+                var dist = self.damlev_distance(substring[j], q_t[j]);
+                var norm = Math.max(substring[j].length, q_t[j].length);
+                local_distance += dist/norm;
+            }
+            if(local_distance/short < 1-self.options.min_similarity) {
+                similarity = 1-(local_distance/short);
+                break;
+            }
+        }
+        return similarity;
+    }
 
-	self.compare = function(key, query) {
-		// Takes into account comparison operators (n-gram, edit distance)
-		var gram = self.n_gram(key, query);
-		var edit = self.edit_similarity(key, query);
+    self.compare = function(key, query) {
+        // Takes into account comparison operators (n-gram, edit distance)
+        var gram = self.n_gram(key, query);
+        var edit = self.edit_similarity(key, query);
 
-		// Prioritize n-gram because edits only account for misspellings
-		return edit*0.35 + gram*0.65;
-	}
+        // Prioritize n-gram because edits only account for misspellings
+        return edit*0.35 + gram*0.65;
+    }
 
-	self.search = function(dict, query) {
-		var matches = [];
-		if(query.length < self.options.min_query) return dict;
+    self.search = function(dict, query) {
+        var matches = [];
+        if(query.length < self.options.min_query) return dict;
 
-		// Matches string against members of a dictionary
-		for(var i = 0; i < dict.length; i++) {
-			var current = dict[i];
-			var s = self.compare(current, query);
-			
-			if(s > self.options.min_similarity) {
-				matches.push(current);
-			}
-		}
+        // Matches string against members of a dictionary
+        for(var i = 0; i < dict.length; i++) {
+            var current = dict[i];
+            var s = self.compare(current, query);
+            
+            if(s > self.options.min_similarity) {
+                matches.push(current);
+            }
+        }
 
-		if(self.options.sort) {
-			matches.sort(function(a, b) {
-				var sA = self.compare(a, query);
-				var sB = self.compare(b, query);
-				if(sA < sB) return 1;
-				if(sB < sA) return -1;
-				return 0;
-			});
-		}
+        if(self.options.sort) {
+            matches.sort(function(a, b) {
+                var sA = self.compare(a, query);
+                var sB = self.compare(b, query);
+                if(sA < sB) return 1;
+                if(sB < sA) return -1;
+                return 0;
+            });
+        }
 
-		if(matches.length == 0) return dict;
+        if(matches.length == 0) return dict;
 
-		if(self.options.all_matches) {
-			return matches;
-		}
-		else {
-			return [matches[0]];
-		}
-	}
+        if(self.options.all_matches) {
+            return matches;
+        }
+        else {
+            return [matches[0]];
+        }
+    }
 }
